688e8378e59bc9edd5d91e7a3d0de2f0a04f1042
Svetlana <florina33@gmail.com>
2019-03-27 13:57:38 +0200

    remove comments

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
-import $ from 'jquery';
 
 let dataUtl = 'https://www.mocky.io/v2/55f748b33568195d044b3dc8';
-
-let userListAll = $('.user-list-all');
-let userListActive = $('.user-list-active');
-let userListAgeSorted = $('.user-list-age-sorted');
-let userListNameSorted = $('.user-list-name-sorted');
-let userListSurnameLimit = $('.user-list-surname-sorted');
 let tableTemplate;
-let surnameLimitlength = 5;
+let surnameLimitLength = 6;
 
+const userListAll = document.querySelector('.user-list-all');
+const userListActive = document.querySelector('.user-list-active');
+const userListAgeSorted = document.querySelector('.user-list-age-sorted');
+const userListNameSorted = document.querySelector('.user-list-name-sorted');
+const userListSurnameLimit = document.querySelector('.user-list-surname-sorted');
+
+
+async function getUsers (requestUrl) {
+	const response = await axios.get(requestUrl);
+
+	return response.data;
+}
 
 function isActive (element) {
 	return element.isActive;
@@ -27,7 +32,7 @@ function compareName (personA, personB) {
 }
 
 function isLongSurname (element) {
-	return element.name.last.length > surnameLimitlength;
+	return element.name.last.length >= surnameLimitLength;
 }
 
 function toRowHtml (item) {
@@ -48,42 +53,11 @@ function toRowHtml (item) {
 	return tableTemplate;
 }
 
-/*
-insertAdjacentHTML() разбирает указанный текст как HTML или XML и вставляет полученные узлы (nodes) в DOM дерево в указанную позицию. Данная функция не переписывает имеющиеся элементы, что предовращает дополнительную сериализацию
-и поэтому работает быстрее, чем манипуляции с innerHTML.
-element.insertAdjacentHTML(position, text);
-position указывает положение element, и может принимать одно из следующих значений:...
-'beforeend' - Внутри element, после последнего дочернего элемента.
-text -  строка, которая будет проанализирована как HTML или XML и вставлена в DOM дерево документа.
-*/
 function renderTo (typeTable) {
 	return function (element) {
 		typeTable.insertAdjacentHTML('beforeEnd', element);
 	};
 }
-
-
-// async function getUsers () {
-// 	const response = await axios.get('https://www.mocky.io/v2/55f748b33568195d044b3dc8');
-
-// 	return response.data;
-// }
-
-function getUsers (requestUrl) {
-
-	const response = axios.get(requestUrl);
-
-	// .then(function done (response) {
-
-	// 	return response.data;
-	// })
-	// .catch(function (error) {
-	// 	console.log(error);
-	// });
-
-	return response.data;
-}
-
 
 getUsers(dataUtl).then(function (users) {
 	users
@@ -114,30 +88,3 @@ getUsers(dataUtl).then(function (users) {
 
 	return users;
 }).catch(console.error);
-
-
-/*
-forEach
-Метод «arr.forEach(callback[, thisArg])» используется для перебора массива.
-
-Он для каждого элемента массива вызывает функцию callback.
-
-Этой функции он передаёт три параметра callback(item, i, arr):
-
-Метод forEach ничего не возвращает, его используют только для перебора, как более «элегантный» вариант, чем обычный цикл for.
-
-filter
-Метод «arr.filter(callback[, thisArg])» используется для фильтрации массива через функцию.
-Он создаёт новый массив, в который войдут только те элементы arr, для которых вызов callback(item, i, arr) возвратит true.
-
-map
-Метод «arr.map(callback[, thisArg])» используется для трансформации массива.
-Он создаёт новый массив, который будет состоять из результатов вызова callback(item, i, arr) для каждого элемента arr.
-
-forEach – для перебора массива.
-filter – для фильтрации массива.
-every/some – для проверки массива.
-map – для трансформации массива в массив.
-reduce/reduceRight – для прохода по массиву с вычислением значения.
-
-*/
